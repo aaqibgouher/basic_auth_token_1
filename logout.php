957b5723d8e4d8ef65360207ef2d49b2ccc7dc0b
aaqibgouher <aaqibgouher@gmail.com>
2020-08-20 16:48:24 +0530

    added comments in the files

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-	include "header.php";
-	session_unset();	
-	header("location: index.php");
+	include "header.php";		
+	session_unset();		/*this function, removed the values from $_SESSION .*/
+	header("location: index.php");		/*so after logout , we are redirecting the user in index page . from where user can either login or register .*/
 ?>	
